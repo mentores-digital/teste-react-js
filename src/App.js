dbb4ccc58b12e5f9dbeafdc432d098ac21da5d0b
Jhonatan Faustinoni <jhonatanwesley.faustinoni@gmail.com.br>
2020-07-13 11:15:21 -0300

    Testando comunicação com a api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import { Button } from 'reactstrap';
+import api from './services/api';
 
-function App() {
-  return (
-    <h1>Testando bootstrap</h1>
-  );
-}
+// function App() {
+//   return (
+//     <h1>Testando bootstrap</h1>
+//   );
+// }
+
+// export default App;
+
+
+class App extends Component {
+
+  state = {
+    products: [],
+  }
+
+  async componentDidMount() {
+    const response = await api.get('/products');
+
+    this.setState({ products: response.data });
+  }
+
+  render() {
+
+    const { products } = this.state;
+
+    return (
+      <div>
+        <h1>Listar os Filmes</h1>
+        {products.map(product => (
+          <li key={product.id}>
+            <h2>
+              <strong>Título: </strong>
+              {product.title}
+            </h2>
+            <p>
+              {product.url}
+            </p>
+            <img src={product.picture}/>
+
+          </li>
+        ))}
+      </div>
+    );
+  };
+};
 
 export default App;
