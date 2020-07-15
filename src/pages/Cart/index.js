bb2e65001ce7029201869d4ae654d5ba3bc388c7
Jhonatan Faustinoni <jhonatanwesley.faustinoni@gmail.com.br>
2020-07-15 19:51:18 -0300

    Layout carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { formatPrice } from '../../util/format';
-import { Container, Table} from 'reactstrap';
+import { Container, Table, Jumbotron } from 'reactstrap';
 import * as CartActions from '../../store/modules/cart/actions';
+import { FaTrash, FaMinus, FaPlus } from "react-icons/fa";
 
 export default function Cart() {
-  const total = useSelector( state =>
+  const total = useSelector(state =>
     formatPrice(
       state.cart.reduce((totalsum, product) => {
         return totalsum + product.price * product.amount;
       }, 0)
-    )  
+    )
   );
 
-  const cart = useSelector(state => 
+  const cart = useSelector(state =>
     state.cart.map(product => ({
       ...product,
       subTotal: formatPrice(product.price * product.amount),
@@ -23,63 +24,65 @@ export default function Cart() {
   const dispatch = useDispatch();
 
   function increment(product) {
-    dispatch(CartActions.updateAmountRequest(product.id, product.amount + 1));
+    dispatch(CartActions.updateAmountSuccess(product.id, product.amount + 1));
   }
 
-  function decrement(product){
-    dispatch(CartActions.updateAmountRequest(product.id, product.amount - 1));
+  function decrement(product) {
+    dispatch(CartActions.updateAmountSuccess(product.id, product.amount - 1));
   }
   return (
-    <Container>
-      <Table hover>
-      <thead>
-        <tr>
-            <th>Imagem Produto</th>
-            <th>Produto</th>
-            <th>Qtd</th>
-            <th>Subtotal</th>
-        </tr>
-      </thead>
-      <tbody>
-      {cart.map(product => (
+    <div class="cart">
+      <Container>
+        <Table hover bordered>
+          <thead>
             <tr>
-              <td>
-                <img class="img-fluid" src={product.picture} alt={product.title} />
-              </td>
-              <td>
-                {product.title}
-              </td>
-              <td>
-                {product.priceFormatted}
-              </td>
-              <td>
-                <div>
-                  <button type="button" onClick={() => decrement (product)}>
-                    menos
-                  </button>
-                  <input type="number" readOnly value={product.amount} />
-                  <button type="button" onClick={() => increment (product)}>
-                    mais
-                  </button>
-                </div>
-              </td>
-              <td>
-                <strong>{product.subTotal}</strong>
-              </td>
-              <td>
-                <button
-                  type="button"
-                  onClick={() => dispatch(CartActions.removeCart(product.id))}
-                >
-                  delete
-                </button>
-              </td>
+              <th>Imagem Produto</th>
+              <th>Produto</th>
+              <th>Preço</th>
+              <th>Qtd</th>
+              <th>Subtotal</th>
+              <th>Remover</th>
             </tr>
-          ))}
-      </tbody>
-    </Table>
-      <span>Total</span>
-      <strong>{total}</strong>
-    </Container>
+          </thead>
+          <tbody>
+            {cart.map(product => (
+              <tr>
+                <td>
+                  <img class="img-fluid" src={product.picture} alt={product.title} />
+                </td>
+                <td>
+                  {product.title}
+                </td>
+                <td>
+                  {product.priceFormatted}
+                </td>
+                <td>
+                  <div class="quantity">
+                    <button type="button" class="btn" onClick={() => decrement(product)}>
+                      <FaMinus />
+                    </button>
+                    <input type="number" readOnly value={product.amount} />
+                    <button type="button" class="btn" onClick={() => increment(product)}>
+                      <FaPlus />
+                    </button>
+                  </div>
+                </td>
+                <td>
+                  <strong>{product.subTotal}</strong>
+                </td>
+                <td>
+                  <button type="button" class="btn btn-danger " onClick={() => dispatch(CartActions.removeCart(product.id))}>
+                    <FaTrash />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+        <Jumbotron>
+          <h4>Total: <strong>{total}</strong></h4>
+        </Jumbotron>
+      </Container>
+    </div>
   );
 }
